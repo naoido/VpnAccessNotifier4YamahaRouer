f405d5a1e022ed2c1399b6b4e205ec853c484998
naoido <54303857+naoido@users.noreply.github.com>
2023-09-22 17:24:27 +0900

    initial commit

---
+++ vpn_access_notifier.lua
@@ -0,0 +1,33 @@
+--##  http://www.rtpro.yamaha.co.jp/RT/docs/lua/rt_api.html  ##--
+
+--          #    Setting     #          --
+PROJECT_NAME = "VPN_ACCESS_NOTIFIER"
+LOG_MATCH_PATTERN = "Call detected from user"
+WEBHOOK_URL = "https://discord.com/api/webhooks/XXX"
+USER_NAME = "YAMAHA RTX1210"
+ADMIN_DISCORD_ID = "your discord id"
+
+--          #     Logger     #          --
+local function logger(log_level, text)
+    rt.syslog(log_level, "["..PROJECT_NAME.."] "..text)
+end
+
+--          #     Watcher    #          --
+while(true) do
+    local rtn, str = rt.syslogwatch(LOG_MATCH_PATTERN)
+
+    if rtn and str then
+        local nowTime = os.date("%y/%m/%d %H:%M:%S")
+        local response = rt.httprequest({
+            url = WEBHOOK_URL,
+            method = "POST",
+            post_text = '{"content": "<@!'..ADMIN_DISCORD_ID..'> VPNに接続がありました！`'..nowTime..'`\\n'..str[1]..'","username": "'..USER_NAME..'"}',
+            content_type = "application/json"
+        })
+        if response.err then
+            logger("info", "error response: " .. response.err)
+        else
+            rt.syslog("info", "A notification has been sent.")
+        end
+    end
+end 
